85be1f6bd9f6b2ff5ad47f0a302564b3da9ff2e9
Juan Manuel B P <pro18qwe@gmail.com>
2024-11-17 22:19:13 +0100

    chore: añadir configuración de Firebase Hosting y GitHub Actions
    - Crear archivo `src/firebase/firebase.js` para gestionar la inicialización y configuración del SDK de Firebase.
    - Crear archivo `.firebaserc` para configuración del proyecto en Firebase.
    - Configurar `firebase.json` para definir los parámetros de despliegue en Firebase Hosting.
    - Agregar script de despliegue en `package.json` para facilitar el despliegue a Firebase Hosting.
    - Actualizar `package-lock.json` y `package.json` con las dependencias necesarias para el despliegue.
    - Añadir archivos de configuración de GitHub Actions:
    - `.github/workflows/firebase-hosting-merge.yml` para despliegue en la rama principal.
    - `.github/workflows/firebase-hosting-pull-request.yml` para despliegue en solicitudes de pull request.

---
+++ src/firebase/firebase.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCSku9CiotGaJSzdykKeezx7Oj3PBcn6gQ",
+  authDomain: "juanmanueldev-1ba80.firebaseapp.com",
+  projectId: "juanmanueldev-1ba80",
+  storageBucket: "juanmanueldev-1ba80.firebasestorage.app",
+  messagingSenderId: "188371056060",
+  appId: "1:188371056060:web:6d696102643380e814a7b9"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
