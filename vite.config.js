c7839b6e7fba7b93ef64450f959535568364852f
Juan Manuel B P <pro18qwe@gmail.com>
2024-11-18 18:02:34 +0100

    chore: configurar coverage
    - Se configuró el coverage en 'vite.config.js'.
    - Se añadió la carpeta 'coverage' al .gitignore para evitar su
      seguimiento.
    - Se incluyó un script en 'package.json' para ejecutar las pruebas de
      coverage.

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,15 @@ export default defineConfig({
   test: {
     globals: true,
     environment: 'jsdom',
-    setupFiles: './src/tests/setupTests.js'
+    setupFiles: './src/tests/setupTests.js',
+    coverage: {
+      provider: 'v8', // o 'istanbul'
+      reportsDirectory: './coverage',
+      reporter: ['text', 'html'], // Tipos de reportes: texto, HTML, JSON, etc.
+      exclude: [
+        'eslint.config.js','postcss.config.js','tailwind.config.js','vite.config.js','src/firebase/firebase.js',
+      ],
+    },
   },
 });
 
