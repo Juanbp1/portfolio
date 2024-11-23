6a7caa0719a1a48a825eb61265c585e604363fba
Juan Manuel B P <pro18qwe@gmail.com>
2024-11-23 23:50:16 +0100

    feat(molecules): añadir componente HeadingWithIcon

---
+++ src/components/molecules/HeadingWithIcon.jsx
@@ -0,0 +1,23 @@
+import PropTypes from "prop-types";
+
+const HeadingWithIcon = ({ titleColor, hrColor, Icon }) => {
+  return (
+    <div className="flex flex-col items-center">
+      <Icon/>
+      <div className="mt-size-md flex flex-col items-center">
+        <h2 className={`${titleColor} heading2`}>Lorem ipsum</h2>
+        <hr
+          className={`${hrColor} mt-size-md w-36 rounded-full border-t-8`}
+        ></hr>
+      </div>
+    </div>
+  );
+};
+HeadingWithIcon.propTypes = {
+  Icon: PropTypes.elementType.isRequired,
+  titleColor: PropTypes.string,
+  hrColor: PropTypes.string,
+  iconColor: PropTypes.string,
+};
+
+export default HeadingWithIcon;
