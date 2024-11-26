2e134e861c90785e88d2148b5728d070cb5e6418
Juan Manuel B P <pro18qwe@gmail.com>
2024-11-26 20:59:41 +0100

    feat: instalación y configuración inicial de react-router-dom
    - Se configuran las rutas.
    - Se eliminan archivos no utilizados (App.jsx, .empty).

---
+++ src/components/pages/Home.jsx
@@ -0,0 +1,13 @@
+import HeroSection from "../organisms/HeroSection";
+import Header from "../organisms/Header";
+
+const Home = () => {
+  return (
+    <div className="dark:bg-gray-600">
+      <Header />
+      <HeroSection />
+    </div>
+  );
+};
+
+export default Home;
