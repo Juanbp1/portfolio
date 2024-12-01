3feb4311880cc3c0df1917f98908e2fb8b9d4461
Juan Manuel B P <pro18qwe@gmail.com>
2024-12-01 23:54:58 +0100

    style: ajustes en estilos de HeroBanner, Header y HeroSection
    - Refinados colores en `HeroBanner` para modo oscuro.
    - Cambiada proporción de imágenes en `HeroSection` (`w-[37%]`).
    - Actualizado color de relleno en `Header` para consistencia visual.

--- src/components/organisms/Header.jsx
+++ src/components/organisms/Header.jsx
@@ -5,7 +5,7 @@ import DarkModeToogle from "../atoms/DarkModeToogle";
 const Header = () => {
   return (
     <header data-testid="header" className="max-w-header mx-auto flex items-center justify-between">
-      <Logo className="dark:!fill-darkMode-primary-light"/>
+      <Logo className="dark:!fill-darkMode-primary"/>
       <div className="flex w-[56%] justify-between">
         <Nav />
         <DarkModeToogle />
