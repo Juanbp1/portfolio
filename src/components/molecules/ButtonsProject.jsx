9aea54f407688833a54a7a4f46ed6ce82c4efa6d
Juan Manuel B P <pro18qwe@gmail.com>
2024-12-13 10:48:03 +0100

    feat: añadir componente ButtonsProject para manejo de botones de proyecto
    Se crea el componente ButtonsProject, que incluye dos botones con
    estilos y funcionalidades diferenciadas:
    - "Ver en vivo": redirige a la URL proporcionada para ver el proyecto en
      producción.
    - "Caso de estudio": redirige a la URL del caso de estudio.

---
+++ src/components/molecules/ButtonsProject.jsx
@@ -0,0 +1,38 @@
+import { IoMdEye } from "react-icons/io";
+import Buttons from "../atoms/Buttons";
+import { GoArrowUpRight } from "react-icons/go";
+import PropTypes from "prop-types";
+
+const ButtonsProject = ({ liveUrl, caseStudyUrl }) => {
+  const handleRedirect = (url) => {
+    if (url) {
+      window.open(url, "_blank"); // Abre la URL en una nueva pestaña
+    } else {
+      console.error("URL no válida");
+    }
+  };
+  return (
+    <div className="btnContainer flex h-[80%] flex-col items-center justify-evenly">
+      <Buttons
+        className="bg-white text-gray-600 hover:bg-gray-100"
+        label="Ver en vivo"
+        icon={<IoMdEye className="size-size-md text-gray-600" />}
+        size="small"
+        onclick={() => handleRedirect(liveUrl)}
+      />
+      <Buttons
+        className="bg-gray-600 text-white hover:bg-gray-500"
+        label="Caso de estudio"
+        icon={<GoArrowUpRight className="size-size-md text-white" />}
+        size="small"
+        onclick={() => handleRedirect(caseStudyUrl)}
+      />
+    </div>
+  );
+};
+
+ButtonsProject.propTypes = {
+  liveUrl: PropTypes.string.isRequired,
+  caseStudyUrl: PropTypes.string.isRequired,
+};
+export default ButtonsProject;
