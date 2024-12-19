f6a00a75585b2c0747c44f64692a2e4332402181
Juan Manuel B P <110305313+Juanbp1@users.noreply.github.com>
2024-12-19 22:46:29 +0100

    feat(ContactForm): integrar emailJs y mejorar manejo de estado del formulario
    1. Integra EmailJS para enviar formularios directamente desde el frontend.
    2. Mejora el manejo de estados del formulario utilizando el hook useFormStatus para
    mostrar mensajes de carga y éxito/error al enviar el formulario
    3. Modifica el comportamiento del formulario para resetear los datos después de un envío exitoso o fallido.
    4. Deshabilita los campos y el botón de envío mientras se está procesando la solicitud.
    5. Actualiza el nombre del campo 'name' a 'user_name' para cumplir con las convenciones de EmailJS.
    6. Se elimina el prop 'onsubmit'
    7. Se añade nueva imagen ContactForm.png

--- src/components/molecules/ContactForm.jsx
+++ src/components/molecules/ContactForm.jsx
@@ -1,15 +1,18 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
+import emailjs from "@emailjs/browser";
 import Buttons from "../atoms/Buttons";
 import Input from "../atoms/Input";
 import TextArea from "../atoms/TextArea";
-import PropTypes from "prop-types";
+import useFormStatus from "../../hooks/useFormStatus";
 
-const ContactForm = ({ onsubmit }) => {
+const ContactForm = () => {
+  const form = useRef();
   const [formData, setFormData] = useState({
     name: "",
     email: "",
     message: "",
   });
+  const { status, updateStatus, resetStatus } = useFormStatus();
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -18,20 +21,48 @@ const ContactForm = ({ onsubmit }) => {
       [name]: value,
     }));
   };
-
+  const resetForm = () => {
+    setTimeout(() => {
+      form.current.reset();
+      setFormData({ name: "", email: "", message: "" });
+      resetStatus();
+    }, 3000);
+  };
   const handleSubmit = (e) => {
     e.preventDefault();
-    onsubmit(formData);
+    updateStatus(true, "Enviando...");
+    emailjs
+      .sendForm(
+        import.meta.env.VITE_SERVICE_ID,
+        import.meta.env.VITE_TEMPLATE_ID,
+        form.current,
+        import.meta.env.VITE_PUBLIC_KEY,
+      )
+      .then(
+        () => {
+          updateStatus(false, "¡Mensaje Enviado!");
+          resetForm();
+        },
+        () => {
+          updateStatus(false, "¡Error al Enviar!");
+          resetForm();
+        },
+      );
   };
 
   return (
-    <form onSubmit={handleSubmit} className="contactForm dark:bg-gray-600">
+    <form
+      ref={form}
+      onSubmit={handleSubmit}
+      className="contactForm dark:bg-gray-600"
+    >
       <Input
         label="Nombre"
         type="text"
-        name="name"
+        name="user_name"
         value={formData.name}
         onChange={handleChange}
+        disabled={status.isLoading}
       />
       <Input
         label="Email"
@@ -39,6 +70,7 @@ const ContactForm = ({ onsubmit }) => {
         name="email"
         value={formData.email}
         onChange={handleChange}
+        disabled={status.isLoading}
       />
       <TextArea
         label="Mensaje"
@@ -46,12 +78,16 @@ const ContactForm = ({ onsubmit }) => {
         name="message"
         value={formData.message}
         onChange={handleChange}
+        disabled={status.isLoading}
       />
-      <Buttons label="Enviar" className=" mt-size-xs flex justify-center bg-gray-600 dark:bg-light dark:text-gray-500" />
+      <Buttons
+        label={status.statusMessage}
+        type="submit"
+        className="mt-size-xs flex justify-center bg-gray-600 dark:bg-light dark:text-gray-500"
+        disabled={status.isLoading}
+      />
     </form>
   );
 };
-ContactForm.propTypes = {
-  onsubmit: PropTypes.func.isRequired,
-};
+
 export default ContactForm;
