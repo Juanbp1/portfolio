376c009c2f7039ee66290ad0e2a4393c563c3a46
Juan Manuel B P <pro18qwe@gmail.com>
2024-12-22 23:33:36 +0100

    feat: agregar año dinámico en el mensaje de copyright

--- src/components/organisms/Footer.jsx
+++ src/components/organisms/Footer.jsx
@@ -1,13 +1,22 @@
+import { useEffect, useState } from "react";
 import FooterContent from "../molecules/FooterContent";
 
 const Footer = () => {
+  const [currentYear, setCurrentYear] = useState(null);
+  const getCurrentYear = () => new Date().getFullYear();
+
+  useEffect(() => {
+    const year = getCurrentYear();
+    setCurrentYear(year);
+  }, []);
+
   return (
     <div className="bg-red-000 flex flex-col items-center gap-size-2xl p-size-3xl">
       <FooterContent />
       <hr className="w-[70%] divide-solid border-bord-xs border-gray-300" />
       <p className="text3 w-[35%] text-center text-gray-400 dark:text-gray-200">
-        Transformando ideas en interfaces – © 2024 Juan Manuel Barea Pantoja.
-        Todos los derechos reservados.
+        Transformando ideas en interfaces – © {currentYear} Juan Manuel Barea
+        Pantoja. Todos los derechos reservados.
       </p>
     </div>
   );
