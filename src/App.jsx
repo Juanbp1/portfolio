8598a05f8f4bfdb8dc65893214de28a81c28540d
Juan Manuel B P <110305313+Juanbp1@users.noreply.github.com>
2024-12-22 22:58:51 +0100

    feat: añadir footer
    -Se incluye iconos para las redes sociales
    -Texto de copyright
    -Se crea plantilla para un eslogan

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import { BrowserRouter as Router } from "react-router-dom"; // Importa BrowserRouter
 import Header from "./components/organisms/Header"; // Componente Header
+import Footer from "./components/organisms/Footer"
 import AppRoutes from "./routes/AppRouter"; // Importa el archivo de rutas
 import Main from "./components/organisms/Main";
-
 function App() {
   return (
     <Router>
       <Main >
         <Header />
         <AppRoutes />
+        <Footer />
       </Main>
     </Router>
   );
---
+++ src/components/molecules/FooterContent.jsx
@@ -0,0 +1,34 @@
+import Logo from "../../../public/assets/svg/logo.svg?react";
+import ComillaDoble from "../../../public/assets/svg/comilla-doble.svg?react";
+import { TbBrandGithubFilled } from "react-icons/tb";
+import { RiLinkedinFill } from "react-icons/ri";
+import { MdEmail } from "react-icons/md";
+import Link from "../atoms/Link";
+
+const FooterContent = () => {
+  const LINKS = [
+    { icon: <TbBrandGithubFilled />, url: "" },
+    { icon: <RiLinkedinFill />, url: "" },
+    { icon: <MdEmail />, url: "" },
+  ];
+  return (
+    <div className="flex w-[30%] flex-col items-center gap-size-lg text-center">
+      <Logo className="dark:!fill-light" />
+      <div className="relative">
+        <ComillaDoble className="absolute -left-size-lg -top-size-2xl" />
+        <p className="text1 font-semibold dark:text-gray-100">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod
+        </p>
+        <ComillaDoble className="absolute -bottom-size-xl -right-size-xs rotate-180" />
+      </div>
+      <div className="flex gap-size-xl">
+        {LINKS.map((link, index) => (
+          <Link key={index} link={link} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default FooterContent;
