2582fea207dc14c6cdc17b73187d43156ecea415
Juan Manuel B P <110305313+Juanbp1@users.noreply.github.com>
2024-12-23 21:39:00 +0100

    feat: habilitar navegación con anclas del menu nav
    - Se añadió el atributo `id` en cada sección para habilitar el uso
      de anclas.
    - Implementada la funcionalidad de desplazamiento suave y desplazamiento
      central al hacer clic en los enlaces de navegación.
    - Los enlaces de ancla ahora dirigen correctamente a las secciones
      correspondientes: Sobre mí, Tecnologías, Mis proyectos y Contacto.

--- src/components/atoms/NavLink.jsx
+++ src/components/atoms/NavLink.jsx
@@ -1,8 +1,19 @@
 import PropTypes from "prop-types";
 
 const NavLink = ({ to, label }) => {
+  const handleClick = (e) => {
+    e.preventDefault();
+    const targetElement = document.querySelector(to);
+
+    if (targetElement) {
+      targetElement.scrollIntoView({
+        behavior: "smooth",
+        block: "center", // Desplaza al centro de la vista
+      });
+    }
+  };
   return (
-    <a className={`link link:hover link:active dark:active:text-darkMode-primary`} href={to}>
+    <a className={`link link:hover link:active dark:active:text-darkMode-primary`} href={to} onClick={handleClick}>
       {label}
     </a>
   );
