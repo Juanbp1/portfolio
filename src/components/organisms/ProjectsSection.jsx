0fa24fabe91d13f24bf4d9647e4a8f930892af62
Juan Manuel B P <110305313+Juanbp1@users.noreply.github.com>
2025-01-24 21:17:27 +0100

    feat(ProjectsSection):añadir dinámica para proyectos visibles y boton 'Ver más'
    -Implementar el estado projectsToShow con un valor inicial de 3 en el componente ProjectsSection
    
    -Añadir función showMoreProjects para aumentar el número de proyectos visibles.
    
    -Ajustar el componente ProjectContainer para aceptar la prop 'projectsToShow'
    
    -Condicional para mostrar el botón 'Ver más' solo si hay proyectos restantes
    
    -Actualizar proyectos de ejemplo en src/data/projects.js para incluir más entradas.

--- src/components/organisms/ProjectsSection.jsx
+++ src/components/organisms/ProjectsSection.jsx
@@ -3,10 +3,19 @@ import corona from "../../../public/assets/svg/corona.svg?react";
 import ProjectContainer from "./ProjectContainer";
 import Buttons from "../atoms/Buttons";
 import { HiArrowLongRight } from "react-icons/hi2";
+import { useState } from "react";
 
 const ProjectsSection = () => {
+  const [projectsToShow, setProjectsToShow] = useState(3); // Número inicial de proyectos a mostrar
+  const showMoreProjects = () => {
+    setProjectsToShow((prevState) => prevState + 3); // Aumentar el número de proyectos a mostrar
+  };
+
   return (
-    <div className="flex flex-col bg-primary-light pb-size-5xl pt-size-5xl dark:bg-gray-500" id="myProjects">
+    <div
+      className="flex flex-col bg-primary-light pb-size-5xl pt-size-5xl dark:bg-gray-500"
+      id="myProjects"
+    >
       <div className="mx-auto flex w-full max-w-container flex-col gap-size-2xl">
         <HeadingWithIcon
           titleColor="text-gray-600 dark:text-white"
@@ -15,14 +24,17 @@ const ProjectsSection = () => {
           iconColor="dark:fill-accent dark:stroke-accent"
         />
 
-        <ProjectContainer />
-        <Buttons
-          className="mx-auto bg-primary text-white hover:bg-primaryDark dark:bg-darkMode-primary dark:text-light dark:hover:bg-darkMode-secondary"
-          label="Ver mas"
-          icon={
-            <HiArrowLongRight className="size-size-md text-white dark:text-light" />
-          }
-        />
+        <ProjectContainer projectsToShow={projectsToShow} />
+        {projectsToShow <= 3 && (
+          <Buttons
+            className="mx-auto bg-primary text-white hover:bg-primaryDark dark:bg-light dark:text-gray-600"
+            label="Ver mas"
+            icon={
+              <HiArrowLongRight className="size-size-md text-white dark:text-gray-600" />
+            }
+            onclick={showMoreProjects}
+          />
+        )}
       </div>
     </div>
   );
