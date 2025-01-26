b09278986205bec8d473d7e56e130ce168e616a5
Juan Manuel B P <pro18qwe@gmail.com>
2025-01-26 16:40:01 +0100

    chore: eliminar comentario innecesario

--- src/tests/ButtonsProject.test.jsx
+++ src/tests/ButtonsProject.test.jsx
@@ -1,5 +1,5 @@
 import { render, screen, fireEvent } from '@testing-library/react';
-import ButtonsProject from '../components/molecules/ButtonsProject'; // Ajusta la ruta de importación según tu estructura
+import ButtonsProject from '../components/molecules/ButtonsProject';
 import { describe, expect, it, vi } from 'vitest';
 
 describe('Verificar que funciona el componente ButtonsProject', () => {
