96f52e0285f499c88671741ac56ed7c974cbb2ff
Juan Manuel B P <pro18qwe@gmail.com>
2025-02-23 13:49:38 +0100

    refactor: se simplifica la estructura del componente Link

--- src/components/atoms/Link.jsx
+++ src/components/atoms/Link.jsx
@@ -2,16 +2,14 @@ import PropTypes from "prop-types";
 
 const Link = ({ link }) => {
   return (
-    <div className="flex h-size-2xl w-size-2xl items-center justify-center rounded-full bg-primary dark:bg-gray-100">
-      <a
-        href={link.url}
-        target="_blank"
-        rel="noopener noreferrer"
-        className="text-3xl text-light transition-colors dark:text-gray-600"
-      >
-        {link.icon}
-      </a>
-    </div>
+    <a
+      href={link.url}
+      target="_blank"
+      rel="noopener noreferrer"
+      className="text-3xl text-light transition-colors dark:text-gray-600"
+    >
+      {link.icon}
+    </a>
   );
 };
 Link.propTypes = {
