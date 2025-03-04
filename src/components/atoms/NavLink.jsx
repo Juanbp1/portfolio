dd665bd0185e1c39b40198c57c2ab6d31b49c65c
Juan Manuel B P <jbareapantoja@gmail.com>
2025-03-04 18:24:25 +0100

    feat: Implementar función de desplazamiento suave
    -Agregar la utilidad de desplazamiento suave smoothScroll en la carpeta utils
    
    -Se actualiza el HeroBanner y el NavLink para incorporar el smoothScroll
    
    -Eliminar archivo .empty

--- src/components/atoms/NavLink.jsx
+++ src/components/atoms/NavLink.jsx
@@ -1,19 +1,13 @@
 import PropTypes from "prop-types";
+import smoothScroll from "../../utils/smoothScroll";
 
 const NavLink = ({ to, label }) => {
-  const handleClick = (e) => {
-    e.preventDefault();
-    const targetElement = document.querySelector(to);
-
-    if (targetElement) {
-      targetElement.scrollIntoView({
-        behavior: "smooth",
-        block: "center", // Desplaza al centro de la vista
-      });
-    }
-  };
   return (
-    <a className={`link link:hover link:active dark:active:text-darkMode-primary`} href={to} onClick={handleClick}>
+    <a
+      className={`link:hover link:active link dark:active:text-darkMode-primary`}
+      href={to}
+      onClick={(e) => smoothScroll(e, to)}
+    >
       {label}
     </a>
   );
