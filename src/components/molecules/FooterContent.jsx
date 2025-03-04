0e1356bd2bed494c3028f8662779b26d67e00c9a
Juan Manuel B P <110305313+Juanbp1@users.noreply.github.com>
2025-03-04 18:47:12 +0100

    style(FooterContent): escalar elementos decorativos

--- src/components/molecules/FooterContent.jsx
+++ src/components/molecules/FooterContent.jsx
@@ -8,12 +8,12 @@ const FooterContent = () => {
     <div className="flex w-[30%] flex-col items-center gap-size-lg text-center">
       <Logo className="dark:!fill-light" />
       <div className="relative">
-        <ComillaDoble className="absolute -left-size-lg -top-size-2xl" />
+        <ComillaDoble className="absolute -left-size-lg -top-size-2xl scale-90" />
         <p className="text1 font-semibold dark:text-gray-100">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
           eiusmod
         </p>
-        <ComillaDoble className="absolute -bottom-size-xl -right-size-xs rotate-180" />
+        <ComillaDoble className="absolute -bottom-size-xl -right-size-xs rotate-180 scale-90" />
       </div>
       <SocialNetworks bgColor="bg-primary" />
     </div>
