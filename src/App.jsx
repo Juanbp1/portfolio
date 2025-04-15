f039e36d93955afb801c3f8e9c81a01d68fe9c91
Juan Manuel B P <110305313+Juanbp1@users.noreply.github.com>
2025-04-15 20:37:07 +0200

    feat: agrega soporte para HeaderMobile y mejora lógica de scroll en el Header
    - Se añadió el componente HeaderMobile en App.jsx para gestionar la
      visibilidad del menú en dispositivos móviles.
    - Se implementó el estado `isOpenMenu` en App.jsx para manejar la
      apertura y cierre del menú móvil.
    - Se refactorizó la lógica de scroll en Header.jsx utilizando el
      hook `useScrollDirection`.
    - Se aplicaron animaciones al Header basadas en la dirección del
      scroll (`animate-slideInY` y `animate-slideOutY`).
    - Se actualizó el componente Main para manejar la clase
     `backdrop-modal` cuando el menú está abierto.
     - Se corrigió la sintaxis en algunos archivos.

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,24 @@
 import { BrowserRouter as Router } from "react-router-dom"; // Importa BrowserRouter
 import Header from "./components/organisms/Header"; // Componente Header
-import Footer from "./components/organisms/Footer"
+import Footer from "./components/organisms/Footer";
 import AppRoutes from "./routes/AppRouter"; // Importa el archivo de rutas
 import Main from "./components/organisms/Main";
-function App() {
+import HeaderMobile from "./components/organisms/HeaderMobile";
+import { useState } from "react";
+
+const App = () => {
+  const [isOpenMenu, setIsOpenMenu] = useState(false);
+
   return (
     <Router>
-      <Main >
+      <Main isOpenMenu={isOpenMenu}>
         <Header />
+        <HeaderMobile setIsOpenMenu={setIsOpenMenu} isOpenMenu={isOpenMenu} />
         <AppRoutes />
         <Footer />
       </Main>
     </Router>
   );
-}
+};
 
 export default App;
